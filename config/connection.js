15c514f12a5cd9da611d857f42dd02b82aa54a81
joshtecx <32961912+joshtecx@users.noreply.github.com>
2018-05-14 14:05:00 -0400

    initial folder structuring, schema/ seeds complete, connection.js complete, orm.js in progress

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.DATABASE_PASSWORD || "root",
+    database: "burgers_db"
+});
+
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("Connected as ID: " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name)
+VALUES("Beyond Burger"),
+("Impossible Burger"),
+("Boca Burger");
